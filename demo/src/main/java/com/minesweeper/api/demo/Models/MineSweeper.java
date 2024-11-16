17d6e4a83c5bba2bb88492d222fe47d21617410f
Shersula <samovarov2002@mail.ru>
2024-11-16 22:24:26 +0300

    small refactor

--- demo/src/main/java/com/minesweeper/api/demo/Models/MineSweeper.java
+++ demo/src/main/java/com/minesweeper/api/demo/Models/MineSweeper.java
@@ -46,31 +46,20 @@ public class MineSweeper
 		this.height = height;
 		this.mines_count = mines_count;
 
-		field = new ArrayList<>();
-
-		for(int i = 0; i < height; i++)
-		{
-			List<Character> row = new ArrayList<>();
-
-			for(int j = 0; j < width; j++) row.add(' ');
-
-			field.add(row);
-		}
-
-		setMine();
+		initField();
 
 		game_id = UUID.randomUUID();
 	}
 
 	public boolean click(int col, int row)
 	{
-		if(field.get(row).get(col) != ' ') return false;
+		if(!isCloseCell(col, row)) return false;
 
-		if(checkMine(col, row)) complete('X');
+		if(checkMine(col, row)) complete(CellMark.ExplodeBomb.getMark());
 		else
 		{
 			open(col, row);
-			if(getCountCloseCell() == MinePosition.size()) complete('M');
+			if(getCountCloseCell() == MinePosition.size()) complete(CellMark.NeutralizedBomb.getMark());
 		}
 
 		return true;
@@ -78,12 +67,10 @@ public class MineSweeper
 
 	private void open(int col, int row)
 	{
-		if(col >= width || row >= height || col < 0 || row < 0) return;
-		if(field.get(row).get(col) != ' ') return;
+		if(col >= width || row >= height || col < 0 || row < 0 || !isCloseCell(col, row)) return;
 
 		int MineCount = getCountMineNear(col, row);
-
-		field.get(row).set(col, (char)(MineCount+'0'));
+		setCell(col, row, MineCount);
 
 		if(MineCount == 0)
 		{
@@ -103,16 +90,24 @@ public class MineSweeper
 		return (MinePosition.stream().filter(p -> p.getCol() == col && p.getRow() == row).count() > 0);
 	}
 
+	private void setCell(int col, int row, char value)
+	{
+		field.get(row).set(col, value);
+	}
+
+	private void setCell(int col, int row, int value)
+	{
+		field.get(row).set(col, (char)(value + '0'));
+	}
+
+	private boolean isCloseCell(int col, int row)
+	{
+		return (field.get(row).get(col) == CellMark.CloseCell.getMark());
+	}
+
 	private int getCountMineNear(int col, int row)
 	{
-		int count = 0;
-
-		for(BombPosition p : MinePosition)
-		{
-			if(p.getCol() >= col-1 && p.getCol() <= col+1 && p.getRow() >= row-1 && p.getRow() <= row+1) count++;
-		}
-
-		return count;
+		return (int)MinePosition.stream().filter(p -> p.getCol() >= col-1 && p.getCol() <= col+1 && p.getRow() >= row-1 && p.getRow() <= row+1).count();
 	}
 
 	private int getCountCloseCell()
@@ -121,13 +116,29 @@ public class MineSweeper
 
 		for(List<Character> row : field)
 		{
-			Count += row.stream().filter(c -> c.equals(' ')).count();
+			Count += row.stream().filter(c -> c.equals(CellMark.CloseCell.getMark())).count();
 		}
 
 		return Count;
 	}
 
-	private void setMine()
+	private void initField()
+	{
+		field = new ArrayList<>();
+
+		for(int i = 0; i < height; i++)
+		{
+			List<Character> row = new ArrayList<>();
+
+			for(int j = 0; j < width; j++) row.add(CellMark.CloseCell.getMark());
+
+			field.add(row);
+		}
+
+		initMine();
+	}
+
+	private void initMine()
 	{
 		MinePosition = new HashSet<>();
 		Random random = new Random();
@@ -137,14 +148,11 @@ public class MineSweeper
 
 	private void complete(char MineMark)
 	{
-		for(BombPosition p : MinePosition)
-		{
-			field.get(p.getRow()).set(p.getCol(), MineMark);
-		}
+		for(BombPosition p : MinePosition) setCell(p.getCol(), p.getRow(), MineMark);
 
 		for(List<Character> row : field)
 		{
-			row.stream().filter(c -> c.equals(' ')).forEach(c -> {
+			row.stream().filter(c -> c.equals(CellMark.CloseCell.getMark())).forEach(c -> {
 				int rowIndx = field.indexOf(row);
 				int colIndx = row.indexOf(c);
 
@@ -153,5 +161,23 @@ public class MineSweeper
 		}
 
 		setCompleted(true);
+	}
+}
+
+enum CellMark
+{
+	ExplodeBomb('X'),
+	NeutralizedBomb('M'),
+	CloseCell(' ');
+
+	private char Mark;
+
+	CellMark(char Mark)
+	{
+		this.Mark = Mark;
+	}
+
+	public char getMark() {
+		return Mark;
 	}
 }
